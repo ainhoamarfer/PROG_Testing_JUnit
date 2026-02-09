81980495fcbf30814782668dde1b50c25e2177db
ainhoamarfer <ainhoa3mf@gmail.com>
2026-02-09 20:26:36 +0100

    Ejer 3

--- src/test/java/ejercicio03/CalculadoraPrecioFinalTest.java
+++ src/test/java/ejercicio03/CalculadoraPrecioFinalTest.java
@@ -1,6 +1,21 @@
 package ejercicio03;
 
+import org.junit.jupiter.api.Test;
+
 public class CalculadoraPrecioFinalTest {
 
+/*
+Caso nominal con descuento y con IVA.
+Sin descuento (0%).
+Sin IVA (0%).
+Descuento 100% ⇒ resultado 0 (independientemente de IVA).
+Lanza excepción con basePrice < 0.
+Lanza excepción con discountPercent < 0 o > 100.
+Lanza excepción con vatPercent < 0 o > 100.
+Prueba de redondeo: valor que obliga a redondear (p. ej. termina en 3er decimal 5).
+ */
+
+    @Test
+    public void test_calculate_() {}
 
 }
